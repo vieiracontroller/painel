caad77206dd2f6522f5fa405d7658ba8c294e4a9
vieiracontroller <vieiracontroller@gmail.com>
2026-06-26 19:47:57 +0000

    Revert logo to logo.png.png

--- app_gerenciador_tarefas.py
+++ app_gerenciador_tarefas.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import os
 from datetime import datetime
 from calendar import monthrange
 from supabase import create_client, Client
@@ -438,7 +437,7 @@ def obter_perfil_usuario(usuario_email):
 
 
 def render_branding_sidebar():
-    st.sidebar.image("logo1.png", use_container_width=True)
+    st.sidebar.image("logo.png.png", use_container_width=True)
     st.sidebar.markdown(" ")
     st.sidebar.markdown("---")
 
@@ -2397,15 +2396,7 @@ def render_meu_acesso():
 if not st.session_state.logado:
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
-        try:
-            if os.path.exists("logo1.png"):
-                st.image("logo1.png", width=280)
-            elif os.path.exists("logo1.png.png"):
-                st.image("logo1.png.png", width=280)
-            else:
-                st.subheader("V-CONTROLL Hub")
-        except Exception:
-            st.subheader("V-CONTROLL Hub")
+        st.image("logo.png.png", width=280)
         with st.form("form_login"):
             usuario = st.text_input("Usuário ou E-mail")
             senha = st.text_input("Senha", type="password")
